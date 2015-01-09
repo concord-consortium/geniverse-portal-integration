ad7b0b06012da0ae73ed2ae6e8f390f390790031
Aaron Unger <aaron.unger@gmail.com>
2015-01-09 14:59:36 -0800

    Fix SQL for children and siblings relationships on Dragons
    [#85812492]

--- app/models/geniverse/dragon.rb
+++ app/models/geniverse/dragon.rb
@@ -1,8 +1,8 @@
 module Geniverse
   class Dragon < ActiveRecord::Base
     attr_protected :id
-    has_many :children, :class_name => "Dragon", :finder_sql => proc { "SELECT * FROM dragons WHERE mother_id = #{id} OR father_id = #{id}" }
-    has_many :siblings, :class_name => "Dragon", :finder_sql => proc { "SELECT * FROM dragons WHERE (mother_id = #{mother_id} OR father_id = #{father_id}) AND breeder_id = #{breeder_id} AND breedTime = #{breedTime} AND id != #{id}" }
+    has_many :children, :class_name => "Dragon", :finder_sql => proc { "SELECT * FROM #{self.class.table_name} WHERE mother_id = #{id} OR father_id = #{id}" }
+    has_many :siblings, :class_name => "Dragon", :finder_sql => proc { "SELECT * FROM #{self.class.table_name} WHERE (mother_id = #{mother_id} OR father_id = #{father_id}) AND breeder_id = #{breeder_id} AND breedTime = #{breedTime} AND id != #{id}" }
 
     belongs_to :father, :class_name => "Dragon"
     belongs_to :mother, :class_name => "Dragon"
